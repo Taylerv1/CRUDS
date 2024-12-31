3ebd37d4732abb77ff3c09d43b77878cd25cd75d
Taylerv1 <mhmodshhde230@gmail.com>
2024-12-31 20:50:21 +0200

    FINAL EDIT

--- cruds.js
+++ cruds.js
@@ -22,6 +22,9 @@ let count = document.getElementById('count');
 let category = document.getElementById('category');
 let submit = document.getElementById('submit');
 
+let mood = 'create';
+let temp;
+
 
 
 function getTotal() {
@@ -52,20 +55,25 @@ else {
 }
 
 
+showData () ;
+
 submit.onclick = function() {
    
     let products = {
-        title: title.value,
+        title: title.value.toLowerCase(),
         price: price.value,
         taxes: taxes.value,
         ads: ads.value,
         discount: discount.value,
         total: total.innerHTML,
         count: count.value,
-        category: category.value
+        category: category.value.toLowerCase(),
     }
 
+
+   if(title.value != '' && price.value != '' && category.value != '' && products.count < 100) {
     // Count
+    if (mood === 'create') {
     if (products.count > 1){
         for (let i = 0; i < products.count; i++) {
             DataProd.push(products);
@@ -73,13 +81,22 @@ submit.onclick = function() {
     }
     else {
         DataProd.push(products);
+    }}
+
+    else {
+        DataProd[temp] = products;
+        mood = 'create';
+        submit.innerHTML = 'Create';
+        count.style.display = 'block';
+
+
     }
-    
+    clearInput(); //clear input
+}
     localStorage.setItem('products', JSON.stringify(DataProd));
 
+    showData (); //refresh data
     
-    clearInput() //clear input
-    showData () //refresh data
 }
 
 // localStorage.clear();
@@ -106,7 +123,6 @@ function clearInput() {
 
 //read 
 
-showData (); //refresh data
 
 function showData () {
 
@@ -115,7 +131,7 @@ function showData () {
     for (let i = 0; i < DataProd.length; i++) {
         tbody += `
         <tr>
-            <td>${i}</td>
+            <td>${i+1}</td>
             <td>${DataProd[i].title}</td>
             <td>${DataProd[i].price}</td>
             <td>${DataProd[i].taxes}</td>
@@ -125,7 +141,7 @@ function showData () {
             <td>${DataProd[i].count}</td>
             <td>${DataProd[i].category}</td>
             <td>
-            <button id="edit">Edit</button>
+            <button id="edit" onclick="updateItem(${i})">Edit</button>
             <button id="delete" onclick="deleteItem(${i})">Delete</button>
             </td>
             </tr>
@@ -142,6 +158,8 @@ function showData () {
     else {
         DeleteBtn.innerHTML = '';
     }
+
+    getTotal();
 }
 
 
@@ -160,6 +178,28 @@ function deleteItem(i) {
 }
 
 
+//update Item
+
+function updateItem(i) {
+    title.value = DataProd[i].title;
+    price.value = DataProd[i].price;
+    taxes.value = DataProd[i].taxes;
+    ads.value = DataProd[i].ads;
+    discount.value = DataProd[i].discount;
+    total.innerHTML = DataProd[i].total;
+    count.style.display = 'none';   
+    category.value = DataProd[i].category;
+    submit.innerHTML = 'Update';
+    getTotal();
+    mood = 'update';
+    temp = i;
+    scroll({
+        top: 0,
+        behavior: 'smooth',
+    })
+
+}
+
 
 
 // delete all
@@ -168,8 +208,108 @@ function deleteAll() {
     localStorage.clear();
     DataProd.splice(0);
     showData(); //refresh data
+
+    // Code by me
+
+    mood = 'create';
+    submit.innerHTML = 'Create';
+    count.style.display = 'block';
+    clearInput();
+    getTotal(); 
 }
 
 
 
+
+
+
+
+// Serach
+
+
+
+let searchMood = 'Title';
+
+function searchButtons(id) {
+
+    let search = document.getElementById('search');
+    SeachEmpty = search;
+
+    if (id === 'searchTitle') {
+        searchMood = 'Title';
+    }
+
+    else {
+        searchMood = 'Category';
+    }
+
+search.placeholder = `Search by ${searchMood}`;
+search.focus();
+search.value = '';
+showData();
+
+}
+
+
+
+
+function search(value) {
+ let tbody = '';
+    for(let i = 0 ; i < DataProd.length; i++) {
+    if (searchMood === 'Title')  {
+         if (DataProd[i].title.includes(value.toLowerCase())) {
+            tbody += `
+        <tr>
+            <td>${i}</td>
+            <td>${DataProd[i].title}</td>
+            <td>${DataProd[i].price}</td>
+            <td>${DataProd[i].taxes}</td>
+            <td>${DataProd[i].ads}</td>
+            <td>${DataProd[i].discount}</td>
+            <td>${DataProd[i].total}</td>
+            <td>${DataProd[i].count}</td>
+            <td>${DataProd[i].category}</td>
+            <td>
+            <button id="edit" onclick="updateItem(${i})">Edit</button>
+            <button id="delete" onclick="deleteItem(${i})">Delete</button>
+            </td>
+            </tr>
+        `
+         }
+    }
+
+
+
+    else{
+        if (DataProd[i].category.includes(value.toLowerCase())) {
+            tbody += `
+        <tr>
+            <td>${i}</td>
+            <td>${DataProd[i].title}</td>
+            <td>${DataProd[i].price}</td>
+            <td>${DataProd[i].taxes}</td>
+            <td>${DataProd[i].ads}</td>
+            <td>${DataProd[i].discount}</td>
+            <td>${DataProd[i].total}</td>
+            <td>${DataProd[i].count}</td>
+            <td>${DataProd[i].category}</td>
+            <td>
+            <button id="edit" onclick="updateItem(${i})">Edit</button>
+            <button id="delete" onclick="deleteItem(${i})">Delete</button>
+            </td>
+            </tr>
+        `
+         }
+    }
+
+    
+}
+
+document.getElementById('tbody').innerHTML = tbody;
+
+}
+
+
+
+// Finish
 
