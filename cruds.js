d775cad972c3325a1450f53837743537e7181f1a
Taylerv1 <mhmodshhde230@gmail.com>
2024-12-31 15:09:40 +0200

    THIRD EDIT

--- cruds.js
+++ cruds.js
@@ -1,11 +1,11 @@
-// get total
-// Create product
-// Save local storage   
-// clear input
-// read
+// get total     #
+// Create product      #
+// Save local storage       #
+// clear input      #
+// read      #
 // count
-// delete
-// update
+// delete Item      #
+// update Item
 // search
 // clean data
 
@@ -37,21 +37,24 @@ function getTotal() {
 }
    
 
+
+
+
 // Create product
 
-let Dataprod ;
+let DataProd ;
 
-if (localStorage.Product != null) {
-    Dataprod = JSON.parse(localStorage.getItem('Product'));
+if (localStorage.products != null) { 
+    DataProd = JSON.parse(localStorage.getItem('products'));
 }
 else {
-    Dataprod = [];
+    DataProd = [];
 }
-
 
 
 submit.onclick = function() {
-    let newprod = {
+   
+    let products = {
         title: title.value,
         price: price.value,
         taxes: taxes.value,
@@ -61,6 +64,112 @@ submit.onclick = function() {
         count: count.value,
         category: category.value
     }
-    Dataprod.push(newprod);
-    localStorage.setItem('Product',JSON.stringify(Dataprod));
+
+    // Count
+    if (products.count > 1){
+        for (let i = 0; i < products.count; i++) {
+            DataProd.push(products);
+        }
+    }
+    else {
+        DataProd.push(products);
+    }
+    
+    localStorage.setItem('products', JSON.stringify(DataProd));
+
+    
+    clearInput() //clear input
+    showData () //refresh data
 }
+
+// localStorage.clear();
+
+
+
+//clear input
+
+function clearInput() {
+    title.value = '';
+    price.value = '';
+    taxes.value = '';
+    ads.value = '';
+    discount.value = '';
+    total.innerHTML = '';
+    count.value = '';
+    category.value = '';
+}
+
+
+
+
+
+
+//read 
+
+showData (); //refresh data
+
+function showData () {
+
+    let tbody = '';
+
+    for (let i = 0; i < DataProd.length; i++) {
+        tbody += `
+        <tr>
+            <td>${i}</td>
+            <td>${DataProd[i].title}</td>
+            <td>${DataProd[i].price}</td>
+            <td>${DataProd[i].taxes}</td>
+            <td>${DataProd[i].ads}</td>
+            <td>${DataProd[i].discount}</td>
+            <td>${DataProd[i].total}</td>
+            <td>${DataProd[i].count}</td>
+            <td>${DataProd[i].category}</td>
+            <td>
+            <button id="edit">Edit</button>
+            <button id="delete" onclick="deleteItem(${i})">Delete</button>
+            </td>
+            </tr>
+        `
+    }
+
+    document.getElementById('tbody').innerHTML = tbody;
+
+    //Delete All Button
+    let DeleteBtn = document.getElementById('DeleteAll');
+    if (DataProd.length > 0) {
+        DeleteBtn.innerHTML = `<button onclick='deleteAll()' class='Bigbtn'>Delete All (${DataProd.length})</button>`
+    }
+    else {
+        DeleteBtn.innerHTML = '';
+    }
+}
+
+
+
+
+
+
+
+
+//delete Item
+
+function deleteItem(i) {
+    DataProd.splice(i ,1);
+    localStorage.products = JSON.stringify(DataProd);
+    showData(); //refresh data
+}
+
+
+
+
+// delete all
+
+function deleteAll() {
+    localStorage.clear();
+    DataProd.splice(0);
+    showData(); //refresh data
+}
+
+
+
+
